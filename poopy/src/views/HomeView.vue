760ff5ff7a22012fa2ff481929ccd3c54abf419a
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-05-09 13:58:38 -0400

    sadfasdfasdfasdfdasfasdfasd

---
+++ poopy/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>login to our very cool bookstore</h1>
+    <h2>username</h2>
+    <input type="text" />
+    <h2>password</h2>
+    <input type="text" />
+
+    <button>login yay</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  margin-bottom: 30px;
+  font-size: 40px;
+}
+
+input {
+  margin-bottom: 10px;
+}
+</style>
